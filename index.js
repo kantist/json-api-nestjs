22d05a004693a792991fa9745ad8c74c6d0d831a
emiryumak <emiryumak@gmail.com>
2022-07-15 16:26:16 +0300

    Fixes

---
+++ index.js
@@ -0,0 +1,11 @@
+export * from './src/lib/json-api.module';
+export * from './src/lib/constants/reflection';
+export * from './src/lib/json-api.module';
+export * from './src/lib/decorators';
+export * from './src/lib/constants';
+export * from './src/lib/helpers';
+export * from './src/lib/types';
+export * from './src/lib/mixins';
+export { SwaggerService } from './src/lib/services/swagger/swagger.service';
+export { excludeMethod } from './src/lib/config/bindings';
+//# sourceMappingURL=index.js.map
