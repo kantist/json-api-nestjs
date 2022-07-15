2856a9654f2657b0d88b39f103d0c0f6b4ce5a04
emiryumak <emiryumak@gmail.com>
2022-07-15 15:32:59 +0300

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * from './lib/json-api.module';
+export * from './lib/constants/reflection';
+export * from './lib/json-api.module';
+export * from './lib/decorators';
+export * from './lib/constants';
+export * from './lib/helpers';
+export * from './lib/types';
+export * from './lib/mixins';
+export { SwaggerService } from './lib/services/swagger/swagger.service';
+export { excludeMethod } from './lib/config/bindings';
+
---
+++ src/lib/decorators/inject-service/inject-service.decorator.ts
@@ -0,0 +1,9 @@
+import { Inject } from '@nestjs/common';
+
+import { getServiceToken } from '../../helpers';
+
+export function InjectService(): PropertyDecorator {
+	return (target, key) => {
+		Inject(getServiceToken(target.constructor))(target, key);
+	};
+}
